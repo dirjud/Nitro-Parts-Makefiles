59a4b4c9d3301db285684334966b85c475b8ed5c
Lane Brooks <dirjud@gmail.com>
2017-07-18 19:10:06 -0600

    bug fix for exporting DEFS for vivado syn

--- vivado.mk
+++ vivado.mk
@@ -59,6 +59,7 @@ $(FPGA_TOP).bit: vfiles.txt xdcfiles.txt incpaths.txt xcifiles.txt $(THISMAKEFIL
 	TOP=$(FPGA_TOP) PART=$(FPGA_PART) vivado -mode tcl -source $(THISMAKEFILE)vivado.tcl
 
 vfiles.txt: $(SYN_FILES_REL)
+	rm -rf defines.v
 	for x in $(DEFS); do echo '`define' $$x | tr '=' ' ' >> defines.v; done
 	echo defines.v > vfiles.txt
 	for f in $(SYN_FILES_REL); do \
